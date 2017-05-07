62b11793c72519104829a01fa64b951d16b57308
Bongsopha <sophasum02@gmail.com>
2017-05-07 21:58:40 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('/homeindex');
+});
+Route::get('giftoption1/', function () {
+    return view('giftoption');
+});
+Route::get('/pagkage', function () {
+    return view('/packagedetail');
+});
+Route::get('/payment', function() {
+    return view('layout/payment');
+});
+
+Route::get('/hello',function(){
+		return view('giftoption');
+});
